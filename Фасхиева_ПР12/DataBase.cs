eabc72afddf27445e9a013b0da48e64151f3c11e
Дарья Фасхиева <dashaf712@mail.ru>
2022-12-01 15:37:59 +0300

    Добавьте файлы проекта.

---
+++ Фасхиева_ПР12/DataBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Фасхиева_ПР12
+{
+    internal class DataBase
+    {
+        public static travelEntities Base;
+    }
+}
