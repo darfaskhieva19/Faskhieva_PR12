b2263c46dc64530dcbc10d8d955f52e69a1f2116
Дарья Фасхиева <dashaf712@mail.ru>
2022-12-06 15:53:55 +0300

    06/12

--- Фасхиева_ПР12/PageHotels.xaml.cs
+++ Фасхиева_ПР12/PageHotels.xaml.cs
@@ -20,10 +20,14 @@ namespace Фасхиева_ПР12
     /// </summary>
     public partial class PageHotels : Page
     {
+        ClassChange pc = new ClassChange();
         public PageHotels()
         {
             InitializeComponent();
             dgHotel.ItemsSource = DataBase.Base.Hotel.ToList();
+
+            pc.CountPage = DataBase.Base.Hotel.ToList().Count;
+            DataContext = pc;
         }
 
         private void btnAddHotels_Click(object sender, RoutedEventArgs e) 
@@ -48,7 +52,20 @@ namespace Фасхиева_ПР12
 
         private void tbPrev_MouseDown(object sender, MouseButtonEventArgs e)
         {
+            TextBlock tb = (TextBlock)sender;
 
+            switch (tb.Uid)  // определяем, куда конкретно было сделано нажатие
+            {
+                case "prev":
+                    pc.CurrentPage--;
+                    break;
+                case "next":
+                    pc.CurrentPage++;
+                    break;
+                default:
+                    pc.CurrentPage = Convert.ToInt32(tb.Text);
+                    break;
+            }
         }
     }
 }
